61e3ace2b065ddc8c2b5a413c0117b04506d12a6
Tristan Bacon <tristanojbacon@gmail.com>
2016-10-04 11:13:06 +0100

    Remove extra div closing tags from footer

--- footer.php
+++ footer.php
@@ -1,21 +1,14 @@
 
 <div class="footer pure-u-1">
-                <div class="container pure-menu pure-menu-horizontal">
-                  <?php wp_nav_menu( array(
-                    'theme_location' => 'footer_menu',
-                    'container' => '',
-                    'menu_id' => 'navigation',
-                    'menu_class' => 'pure-menu-list' ) );
-                  ?>
-                </div>
-            </div>
-        </div>
-    </div>
+  <div class="container pure-menu pure-menu-horizontal">
+    <?php wp_nav_menu( array(
+      'theme_location' => 'footer_menu',
+      'container' => '',
+      'menu_id' => 'navigation',
+      'menu_class' => 'pure-menu-list' ) );
+    ?>
+  </div>
 </div>
-
-
-
-
 
 <?php wp_footer(); ?>
 </body>
